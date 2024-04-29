b209801b87e74062592cbbc2d79a2c6ce5efaee9
OkohDaniel11 <129377762+OkohDaniel11@users.noreply.github.com>
2024-04-29 13:51:07 +0100

    Added new names to application

--- run.py
+++ run.py
@@ -11,7 +11,7 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive"
     ]
 
-CREDS = Credentials.from_service_account_file('cred.json')
+CREDS = Credentials.from_service_account_file('CRED.JSON')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
